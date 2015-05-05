ecf38f122d3774b9461153cbe3451e505c5473de
Christian Kokott <christian.kokott@gmail.com>
2015-05-05 21:05:39 +0200

    first record

---
+++ RonaldDoNotScratch/Assets/Needle.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Needle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			StartCoroutine(TurnOn());
+		}
+	}
+
+	IEnumerator TurnOn() {
+		iTween.RotateBy(this.gameObject, iTween.Hash("y", -18.0f / 360,
+													"time", 3.0f));
+
+		GameObject.Find("Vinyl").GetComponent<VinylRotate>().StartRotating();
+
+		yield return new WaitForSeconds(3);
+
+		iTween.RotateBy(this.gameObject, iTween.Hash("x", -2.0f / 360,
+													"time", 0.8f));
+
+		yield return new WaitForSeconds(0.2f);
+
+		this.GetComponent<AudioSource>().Play();
+	}
+}
+
---
+++ RonaldDoNotScratch/Assets/VinylRotate.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class VinylRotate : MonoBehaviour {
+
+	public float RotationSpeed;
+
+	private float currentRotationSpeed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(0, currentRotationSpeed * Time.deltaTime, 0);
+	}
+
+	public void StartRotating() {
+		iTween.ValueTo(this.gameObject, iTween.Hash("from", 0.0f,
+													"to", RotationSpeed,
+													"time", 3.0f,
+													"onupdate", "onUpdate"));
+	}
+
+	void onUpdate(float value) {
+		currentRotationSpeed = value;
+	}
+}
